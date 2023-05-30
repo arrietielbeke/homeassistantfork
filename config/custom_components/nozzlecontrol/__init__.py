0285ac3dca658460df273e5c4f0f371c4dd401ff
Arrie Tielbeke <arrietielbeke@gmail.com>
2023-05-30 19:59:38 +0000

    add countdown function

--- config/custom_components/nozzlecontrol/__init__.py
+++ config/custom_components/nozzlecontrol/__init__.py
@@ -1,33 +1,26 @@
 import asyncio
 import logging
 import time
+import datetime
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "nozzlecontrol"
 
-NOZZLENUMBER = "nozzlenumber"
+NOZZLEID = "nozzleid"
 DFNOZZLENUMBER = "1"
-
-TIMERTIME = "timetorun"
 
 
 async def async_setup(hass, config):
     async def handletimer(call):
         _LOGGER.info("Call is made to handleTimer blah blah")
 
-        nozzle_number = call.data.get(NOZZLENUMBER, DFNOZZLENUMBER)
-        timer_time = call.data.get(TIMERTIME, 3)
-
-        epcocht = time.time()
-        datetime = time.ctime(epcocht)
+        nozzle_number = call.data.get(NOZZLEID, DFNOZZLENUMBER)
+        timer_time = hass.states.get("input_number.testnumber")
 
         hass.states.async_set("person.arrie", "teststate")
 
-        await timer(timer_time)
-
-        _LOGGER.info(datetime)
-        _LOGGER.info(nozzle_number)
+        await timer(int(float(timer_time.state)))
 
     hass.services.async_register(DOMAIN, "handletimer", handletimer)
 
@@ -40,5 +33,9 @@ async def async_setup(hass, config):
             await asyncio.sleep(1)
             time_in_seconds -= 1
             _LOGGER.info(time_in_seconds)
+
+            time_to_timestamp = datetime.timedelta(seconds=time_in_seconds)
+
+            hass.states.async_set("input_datetime.countdown", time_to_timestamp)
 
     return True
